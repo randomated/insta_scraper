e3391b7922d81d1a050aa10eac1834b15abdd3f6
Argie Bacomo <argie@bpoc.co.jp>
2023-06-01 14:42:26 +0800

    Switch to headless

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
   info_logger = Logger('info', current_directory)
   saver = Saver(current_directory)
 
-  scraper = InstagramScraper(False, info_logger, saver)
+  scraper = InstagramScraper(True, info_logger, saver)
   scraper.start(links)
 
   scraper.close()
